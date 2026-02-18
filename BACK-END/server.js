a4cefe783791137ccccc60869658312f57799bde
Apurba1818 <apurba2003sarkar2003@gmail.com>
2026-02-18 21:23:32 +0530

    added dynamic port for deployment

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -713,4 +713,9 @@ app.get("/vehicle-profit", verifyToken, (req,res)=>{
 
 /* ================= SERVER ================= */
 
-app.listen(5000, ()=> console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+

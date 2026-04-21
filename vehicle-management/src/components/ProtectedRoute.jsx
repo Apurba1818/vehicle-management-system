16e491b07065f238a619ff33794c85a2220a37cf
Apurba Sarkar <apurba2003sarkar2003@gmail.com>
2026-04-21 23:46:05 +0530

    Update ProtectedRoute.jsx

--- vehicle-management/src/components/ProtectedRoute.jsx
+++ vehicle-management/src/components/ProtectedRoute.jsx
@@ -10,15 +10,28 @@
 
 //   return children;
 // }
+// import { Navigate } from "react-router-dom";
+
+// export default function ProtectedRoute({ children }) {
+
+//   const token = localStorage.getItem("token");
+
+//   if (!token) {
+//     return <Navigate to="/login" />;
+//   }
+
+//   return children;
+// }
+
 import { Navigate } from "react-router-dom";
-
+ 
 export default function ProtectedRoute({ children }) {
-
+ 
   const token = localStorage.getItem("token");
-
+ 
   if (!token) {
     return <Navigate to="/login" />;
   }
-
+ 
   return children;
 }
